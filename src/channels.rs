b50a1292e1742da6e536e81a08b78bcbc3c3f3fd
Justin Bradford <jabradford@gmail.com>
2025-03-24 23:41:31 -0700

    Clean up channel abstraction and management

--- src/channels.rs
+++ src/channels.rs
@@ -16,63 +16,153 @@ enum Channel {
     sender: async_channel::Sender<Entry>,
     receiver: async_channel::Receiver<Entry>,
   },
-  Null {
-    sender: u8,
-    receiver: u8,
+}
+
+fn create(name: &str) -> Channel {
+  if name.starts_with("q/") {
+    let (sender, receiver) = async_channel::bounded::<Entry>(100);
+    return Channel::Queue { sender, receiver };
   }
+
+  let (mut sender, receiver) = async_broadcast::broadcast::<Entry>(100);
+  sender.set_overflow(true);
+  return Channel::Topic { sender, receiver: receiver.deactivate() }
 }
 
-pub trait Receiver {
-  async fn recv(&mut self) -> Result<Entry>;
+enum Subscription {
+  Topic(async_broadcast::Receiver<Entry>),
+  Queue(async_channel::Receiver<Entry>),
 }
 
-impl Receiver for async_broadcast::Receiver<Entry> {
+impl Subscription {
   async fn recv(&mut self) -> Result<Entry> {
-    match self.recv().await {
-      Ok(res) => { Ok(res) }
-      Err(err) => { Err(anyhow!("{:?}", err)) }
+    match self {
+      Self::Topic(receiver) => {
+        receiver.recv().await
+          .map_err(|err| anyhow!("{:?}", err))
+      }
+      Self::Queue(receiver) => {
+        receiver.recv().await
+          .map_err(|err| anyhow!("{:?}", err))
+      }
+    }
+  }
+
+  fn close(&mut self) -> bool {
+    match self {
+      Self::Topic(receiver) => { receiver.close() }
+      Self::Queue(receiver) => { receiver.close() }
     }
   }
 }
 
-impl Channel {
-  // fn subscribe(&self) -> impl Receiver {
-  fn subscribe(&self) -> async_broadcast::Receiver<Entry> {
-    match &self {
-      Channel::Topic { sender: _, receiver } => {
-        receiver.activate_cloned()
-      }
-      _ => { panic!("not implemented") }
-    }
-  }
+enum Publisher {
+  Topic(async_broadcast::Sender<Entry>),
+  Queue(async_channel::Sender<Entry>),
+}
 
+impl Publisher {
   async fn send(&self, entry: Entry) -> Result<()> {
-    match &self {
-      Channel::Topic { sender, receiver: _ } => {
+    match self {
+      Self::Topic(sender) => {
         match sender.broadcast(entry).await {
           Ok(_) => { Ok(()) }
           Err(err) => { Err(anyhow!("{:?}", err)) }
         }
       }
-      Channel::Queue { sender, receiver: _ } => {
+      Self::Queue(sender) => {
         match sender.send(entry).await {
           Ok(_) => { Ok(()) }
           Err(err) => { Err(anyhow!("{:?}", err)) }
         }
       }
-      Channel::Null { sender: _, receiver: _ }=> { Ok(()) }
+    }
+  }
+
+  fn close(&self) -> bool {
+    match self {
+      Self::Topic(sender) => { sender.close() }
+      Self::Queue(sender) => { sender.close() }
     }
   }
 }
 
-fn subscribe_topic(map: &mut HashMap<String, Channel>, name: &str) -> async_broadcast::Receiver<Entry> {
-  map.pin().get_or_insert_with(name.to_string(), || {
-    let (mut sender, receiver) =
-      async_broadcast::broadcast::<Entry>(100);
-    sender.set_overflow(true);
-    Channel::Topic {
-      sender: sender.clone(),
-      receiver: receiver.clone().deactivate(),
+impl Channel {
+  fn subscribe(&self) -> Subscription {
+    match self {
+      Self::Topic { sender: _, receiver } => {
+        Subscription::Topic(receiver.activate_cloned())
+      }
+      Self::Queue { sender: _, receiver } => {
+        Subscription::Queue(receiver.clone())
+      }
     }
-  }).subscribe()
+  }
+
+  fn publisher(&self) -> Publisher {
+    match self {
+      Self::Topic { sender, receiver: _ } => {
+        Publisher::Topic(sender.clone())
+      }
+      Self::Queue { sender, receiver: _ } => {
+        Publisher::Queue(sender.clone())
+      }
+    }
+  }
+
+  fn active(&self) -> bool {
+    match self {
+      Self::Topic { sender, receiver: _ } => {
+        sender.sender_count() > 1 || sender.receiver_count() > 0
+      }
+      Self::Queue { sender, receiver: _ } => {
+        sender.sender_count() > 1 && sender.receiver_count() > 1
+      }
+    }
+  }
+
+  fn close(&self) {
+    match self {
+      Self::Topic { sender, receiver } => {
+        sender.close();
+        receiver.close();
+      }
+      Self::Queue { sender, receiver } => {
+        sender.close();
+        receiver.close();
+      }
+    }
+  }
+}
+
+pub struct Channels {
+  map: HashMap<String, Channel>,
+}
+
+impl Channels {
+  fn new() -> Self {
+    Self { map: HashMap::new() }
+  }
+
+  fn subscribe(&self, name: &str) -> Subscription {
+    return self.map.pin()
+      .get_or_insert_with(name.to_string(), || create(name))
+      .subscribe();
+  }
+
+  fn publisher(&self, name: &str) -> Publisher {
+    return self.map.pin()
+      .get_or_insert_with(name.to_string(), || create(&name))
+      .publisher();
+  }
+
+  fn prune(&self) {
+    let g = self.map.guard();
+    for (name, channel) in self.map.iter(&g) {
+      if !channel.active() {
+        channel.close();
+        self.map.remove(name, &g);
+      }
+    }
+  }
 }
